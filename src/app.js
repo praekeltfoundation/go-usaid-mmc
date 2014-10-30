f041d312fcb0d03888c6d0aa9afa3fbc751f9d3f
gsvr <george@westerncapelabs.com>
2014-10-30 10:31:53 +0200

    implement app flow

--- src/app.js
+++ src/app.js
@@ -1,69 +1,186 @@
 go.app = function() {
     var vumigo = require('vumigo_v02');
+    var _ = require('lodash');
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
     var EndState = vumigo.states.EndState;
 
 
-    // Application flow overview
-    // -------------------------
-
-    // state_start
-    // This will be a switching state, using the following logic:
-    //   - Registered user:
-    //     - 'STOP' > state_opt_out? (opt user out - send message confirming opt-out?)
-    //     - 'any other'
-    //       - Finished receiving msgs > state_finished_msgs (sms to inform they've received all messages)
-    //       - Unfinished msg stream > state_unfinished_msgs (sms to inform they'll receive scheduled msg and how to opt out)
-    //   - Unregistered user:
-    //     - 'MMC' > state_language (send language choice message, register them)
-    //     - 'any other' > state_how_to_register (send sms telling them how to register)
-
-    // state_language
-    // Choice state
-    // 1. Xhosa
-    // 2. Zulu
-    // 3. Sotho
-    // 4. Afrikaans
-    // 5. English
-
-    // state_opt_out
-    // "~~ You have opted out"
-
-    // state_finished_msgs
-    // "~~ You received all messages"
-
-    // state_unfinished_msgs
-    // "~~ You'll receive messages, or sms stop"
-
-    // state_how_to_register
-    // "~~ SMS MMC to ### to register"
-
-
     var GoApp = App.extend(function(self) {
-        App.call(self, 'states:start');
+        App.call(self, 'states_start');
+        var $ = self.$;
 
-        self.states.add('states:start', function(name) {
+        self.init = function() {
+            // self.env = self.im.config.env;
+            // self.metric_prefix = [self.env, self.im.config.name].join('.');
+            // self.store_name = [self.env, self.im.config.name].join('.');
+
+            // self.im.on('session:new', function(e) {
+            //     self.contact.extra.ussd_sessions = go.utils.incr_user_extra(
+            //         self.contact.extra.ussd_sessions, 1);
+            //     self.contact.extra.metric_sum_sessions = go.utils.incr_user_extra(self.contact.extra.metric_sum_sessions, 1);
+
+            //     return Q.all([
+            //         self.im.contacts.save(self.contact),
+            //         self.im.metrics.fire.inc([self.env, 'sum.sessions'].join('.'), 1),
+            //         self.fire_incomplete(e.im.state.name, -1)
+            //     ]);
+            // });
+
+            // self.im.on('session:close', function(e) {
+            //     return Q.all([
+            //         self.fire_incomplete(e.im.state.name, 1),
+            //         self.dial_back(e)
+            //     ]);
+            // });
+
+            // self.im.user.on('user:new', function(e) {
+            //     return Q.all([
+            //         go.utils.fire_users_metrics(self.im, self.store_name, self.env, self.metric_prefix),
+            //         // TODO re-evaluate the use of this metric
+            //         // self.fire_incomplete('states_start', 1)
+            //     ]);
+            // });
+
+            // self.im.on('state:enter', function(e) {
+            //     self.contact.extra.last_stage = e.state.name;
+            //     return self.im.contacts.save(self.contact);
+            // });
+
+            return self.im.contacts
+                .for_user()
+                .then(function(user_contact) {
+                   self.contact = user_contact;
+                });
+        };
+
+
+        self.states.add('states_start', function(name) {
+            var first_word = self.im.msg.content.split(" ")[0].replace(/\W/g, '').toUpperCase();
+
+            // always unsubscribe on stop or block
+            if (first_word === "STOP" || first_word === "BLOCK") {
+                return self.states.create('states_opt_out');
+
+            // user isn't registered
+            } else if (_.isUndefined(self.contact.extra.is_registered) ||
+                            self.contact.extra.is_registered === 'false') {
+                if (first_word === "MMC") {
+                    return self.states.create('states_language');
+                } else {
+                    return self.states.create('states_how_to_register');
+                }
+
+            // user is registered
+            } else if (self.contact.extra.is_registered === 'true') {
+                if (self.contact.extra.finished_messages === 'true') {  // make this happen
+                    return self.states.create('states_finished_messages');
+                } else {
+                    return self.states.create('states_unfinished_messages');
+                }
+            }
+        });
+
+
+        self.states.add('states_language', function(name) {
             return new ChoiceState(name, {
-                question: 'Hi there! What do you want to do?',
+                question:
+                    $("You're registered for messages about your circumcision! Reply with " +
+                    "the number of your chosen language:"),
 
                 choices: [
-                    new Choice('states:start', 'Show this menu again'),
-                    new Choice('states:end', 'Exit')],
+                    new Choice('xh', $("Xhosa")),
+                    new Choice('zu', $("Zulu")),
+                    new Choice('st', $("Sotho")),
+                    new Choice('en', $("English")),
+                    new Choice('af', $("Afrikaans")),
+                ],
 
                 next: function(choice) {
-                    return choice.value;
+                    return self.im.user
+                        .set_lang(choice.value)
+                        .then(function() {
+                            return 'states_start';
+                        });
+                },
+
+                events: {
+                    'state:enter': function() {
+                        self.contact.extra.is_registered = 'true';
+
+                        return self.im.user
+                            .set_lang('en')
+                            .then(function() {
+                                return self.im.contacts.save(self.contact);
+                            })
+                            .then(function() {
+                                // subscribe to english message set
+                            });
+                    }
                 }
             });
         });
 
-        self.states.add('states:end', function(name) {
+        self.states.add('states_how_to_register', function(name) {
             return new EndState(name, {
-                text: 'Thanks, cheers!',
-                next: 'states:start'
+                text:
+                    $("Welcome to the Medical Male Circumcision (MMC) info service. To get " +
+                    "FREE info on how to look after your circumcision wound please SMS 'MMC' " +
+                    "to {{SMS_number}}."
+                    ).context({
+                        SMS_number: self.im.config.channel
+                    }),
+
+                next: 'states_start'
             });
         });
+
+        self.states.add('states_finished_messages', function(name) {
+            return new EndState(name, {
+                text:
+                    $("You have finished your set of SMSs and your circumcision wound should " +
+                    "be healed. If not, please visit your local clinic. Thanks for using " +
+                    "the MMC info service."),
+
+                next: 'states_start'
+            });
+        });
+
+        self.states.add('states_unfinished_messages', function(name) {
+            return new EndState(name, {
+                text:
+                    $("MMC info: U r registered to receive SMSs about ur circumcision. " +
+                    "To opt out SMS 'STOP' to {{SMS_number}}. If u have concerns with ur " +
+                    "wound please visit ur local clinic."
+                    ).context({
+                        SMS_number: self.im.config.channel
+                    }),
+
+                next: 'states_start'
+            });
+        });
+
+        self.states.add('states_opt_out', function(name) {
+            return new EndState(name, {
+                text:
+                    $("You have been unsubscribed."),
+
+                next: 'states_start',
+
+                events: {
+                    'state:enter': function() {
+                        // return go.utils
+                        //     .opt_out(self.im, self.contact)  // make this work
+                        //     .then(function() {
+                        //         return go.utils
+                        //             .subscription_unsubscribe_all(self.contact, self.im, opts);  // make this work
+                        //     });
+                    }
+                }
+            });
+        });
+
     });
 
     return {
--- test/app.test.js
+++ test/app.test.js
@@ -14,59 +14,187 @@ describe("app", function() {
             tester = new AppTester(app);
 
             tester
+                .setup.char_limit(160)
                 .setup.config.app({
-                    name: 'test_app'
+                    name: 'usaid_mmc',
+                    env: 'test',
+                    metric_store: 'usaid_mmc_test',
+                    channel: "555"
                 })
                 .setup(function(api) {
                     fixtures().forEach(api.http.fixtures.add);
+                })
+
+                // Set up contacts
+                .setup(function(api) {
+                    // unregistered user
+                    api.contacts.add({
+                        msisdn: '+082111',
+                        extra: {
+                        }
+                    });
+                })
+                .setup(function(api) {
+                    // registered user message stream complete
+                    api.contacts.add({
+                        msisdn: '+082333',
+                        extra: {
+                            is_registered: 'true',
+                            finished_messages: 'true'
+                        }
+                    });
+                })
+                .setup(function(api) {
+                    // registered user message stream incomplete
+                    api.contacts.add({
+                        msisdn: '+082444',
+                        extra: {
+                            is_registered: 'true'
+                        }
+                    });
                 });
         });
 
-        describe("when the user starts a session", function() {
-            it("should ask them what they want to do", function() {
-                return tester
-                    .start()
-                    .check.interaction({
-                        state: 'states:start',
-                        reply: [
-                            'Hi there! What do you want to do?',
-                            '1. Show this menu again',
-                            '2. Exit'
-                        ].join('\n')
-                    })
-                    .run();
+        describe("when a user wants to unsubscribe", function() {
+            describe("when a registered user sends STOP", function() {
+                it("should unsubscribe them", function() {
+                    return tester
+                        .setup.user.addr('082333')
+                        .input('STOP')
+                        .check.interaction({
+                            state: 'states_opt_out',
+                            reply:
+                                "You have been unsubscribed."
+                        })
+                        .run();
+                 });
+            });
+
+            describe("when an unregistered user sends BLOCK", function() {
+                it("should unsubscribe them", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .input('BLOCK myself')
+                        .check.interaction({
+                            state: 'states_opt_out',
+                            reply:
+                                "You have been unsubscribed."
+                        })
+                        .run();
+                 });
             });
         });
 
-        describe("when the user asks to see the menu again", function() {
-            it("should show the menu again", function() {
-                return tester
-                    .setup.user.state('states:start')
-                    .input('1')
-                    .check.interaction({
-                        state: 'states:start',
-                        reply: [
-                            'Hi there! What do you want to do?',
-                            '1. Show this menu again',
-                            '2. Exit'
-                        ].join('\n')
-                    })
-                    .run();
+        describe("when a user sends in MMC", function() {
+            describe("when the user is unregistered", function() {
+                it("should register them, ask for their language choice", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .input('MMC')
+                        .check.interaction({
+                            state: 'states_language',
+                            reply: [
+                                "You're registered for messages about your circumcision! Reply " +
+                                "with the number of your chosen language:",
+                                "1. Xhosa",
+                                "2. Zulu",
+                                "3. Sotho",
+                                "4. English",
+                                "5. Afrikaans"
+                            ].join('\n')
+                        })
+                        .run();
+                });
+            });
+
+            describe("when the user is registered", function() {
+                describe("if they have received all messages", function() {
+                    it("should tell them they've received all messages", function() {
+                        return tester
+                            .setup.user.addr('082333')
+                            .input('MMC')
+                            .check.interaction({
+                                state: 'states_finished_messages',
+                                reply:
+                                    "You have finished your set of SMSs and your circumcision " +
+                                    "wound should be healed. If not, please visit your local " +
+                                    "clinic. Thanks for using the MMC info service.",
+                            })
+                            .run();
+                    });
+                });
+
+                describe("if they have NOT received all messages", function() {
+                    it("should tell them to wait and how to STOP", function() {
+                        return tester
+                            .setup.user.addr('082444')
+                            .input('MMC')
+                            .check.interaction({
+                                state: 'states_unfinished_messages',
+                                reply:
+                                    "MMC info: U r registered to receive SMSs about ur " +
+                                    "circumcision. To opt out SMS 'STOP' to 555. If u have " +
+                                    "concerns with ur wound please visit ur local clinic.",
+                            })
+                            .run();
+                    });
+                });
+
             });
         });
 
-        describe("when the user asks to exit", function() {
-            it("should say thank you and end the session", function() {
-                return tester
-                    .setup.user.state('states:start')
-                    .input('2')
-                    .check.interaction({
-                        state: 'states:end',
-                        reply: 'Thanks, cheers!'
-                    })
-                    .check.reply.ends_session()
-                    .run();
+        describe("when a user sends in anything except STOP, BLOCK, MMC", function() {
+            describe("when the user is unregistered", function() {
+                it("should tell them how to register", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .input('ouch')
+                        .check.interaction({
+                            state: 'states_how_to_register',
+                            reply:
+                                "Welcome to the Medical Male Circumcision (MMC) info service. " +
+                                "To get FREE info on how to look after your circumcision " +
+                                "wound please SMS 'MMC' to 555."
+                        })
+                        .run();
+                });
+            });
+
+            describe("when the user is registered", function() {
+                describe("if they have received all messages", function() {
+                    it("should tell them they've received all messages", function() {
+                        return tester
+                            .setup.user.addr('082333')
+                            .input('gah')
+                            .check.interaction({
+                                state: 'states_finished_messages',
+                                reply:
+                                    "You have finished your set of SMSs and your circumcision " +
+                                    "wound should be healed. If not, please visit your local " +
+                                    "clinic. Thanks for using the MMC info service.",
+                            })
+                            .run();
+                    });
+                });
+
+                describe("if they have NOT received all messages", function() {
+                    it("should tell them to wait and how to STOP", function() {
+                        return tester
+                            .setup.user.addr('082444')
+                            .input('why')
+                            .check.interaction({
+                                state: 'states_unfinished_messages',
+                                reply:
+                                    "MMC info: U r registered to receive SMSs about ur " +
+                                    "circumcision. To opt out SMS 'STOP' to 555. If u have " +
+                                    "concerns with ur wound please visit ur local clinic.",
+                            })
+                            .run();
+                    });
+                });
             });
         });
+
+
     });
 });
