59437dc4aa657305dc5dd5463f87fdae918907e5
Simon Cross <hodgestar@gmail.com>
2016-03-31 14:19:53 +0200

    Update build USSD app.

--- go-app-ussd.js
+++ go-app-ussd.js
@@ -10,7 +10,9 @@ go.app = function() {
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
+    var FreeText = vumigo.states.FreeText;
     var LanguageChoice = vumigo.states.LanguageChoice;
+    var PaginatedChoiceState = vumigo.states.PaginatedChoiceState;
     var EndState = vumigo.states.EndState;
 
     var GoApp = App.extend(function(self) {
@@ -36,13 +38,19 @@ go.app = function() {
         });
 
         self.states.add('states:main_menu', function(name){
-            return new ChoiceState(name, {
-                question: 'Hi there! What do you want to do?',
-
+            return new PaginatedChoiceState(name, {
+                question: 'Medical Male Circumcision (MMC):',
+                characters_per_page: 160,
+                options_per_page: null,
                 choices: [
-                    new Choice('states:start', 'Show this menu again'),
-                    new Choice('states:end', 'Exit')],
-
+                    new Choice('states:end', 'Find a clinic'),
+                    new Choice('states:end', 'Speak to an expert for FREE'),
+                    new Choice('states:healthsites', 'Get FREE SMSs about your MMC recovery'),
+                    new Choice('states:service_rating_1', 'Rate your clinic\'s MMC service'),
+                    new Choice('states:end', 'Join Brothers for Life'),
+                    new Choice('states:select_language', 'Change Language'),
+                    new Choice('states:end', 'Exit'),
+                ],
                 next: function(choice) {
                     return choice.value;
                 }
@@ -76,6 +84,104 @@ go.app = function() {
                     new Choice("xh", "isiXhosa"),
                     new Choice("ts", "Xitsonga")
                 ]
+            });
+        });
+
+        self.states.add('states:healthsites', function(name){
+            return new ChoiceState(name, {
+                question: "Welcome to Healthsites. What type of clinic are you looking for?",
+                choices: [
+                    new Choice("states:end", "Nearest Clinic"),
+                    new Choice("states:end", "MMC Clinic"),
+                    new Choice("states:end", "HCT Clinic")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_1', function(name){
+            return new FreeText(name, {
+                question: "At which clinic did you get circumcised? Please be specific with the name and " +
+                "location. e.g. Peterville Clinic, Rivonia, Johannesburg",
+                next: function(text) {
+                    return 'states:service_rating_2';
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_2', function(name){
+            return new ChoiceState(name, {
+                question: "Would you recommend a friend to the clinic where you got circumcised?",
+                choices: [
+                    new Choice("states:service_rating_3", "Yes"),
+                    new Choice("states:service_rating_3", "No"),
+                    new Choice("states:service_rating_end2", "I have not been circumcised")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_3', function(name){
+            return new ChoiceState(name, {
+                question: "How would you rate the attitude of the health care workers at the clinic " +
+                "where you got circumcised?",
+                choices: [
+                    new Choice("states:service_rating_4", "Very Bad"),
+                    new Choice("states:service_rating_4", "Bad"),
+                    new Choice("states:service_rating_4", "OK"),
+                    new Choice("states:service_rating_4", "Good"),
+                    new Choice("states:service_rating_4", "Excellent")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_4', function(name){
+            return new ChoiceState(name, {
+                question: "Did you subscribe to the post op SMS service?",
+                choices: [
+                    new Choice("states:service_rating_end1", "Yes I found it helpful"),
+                    new Choice("states:service_rating_end1", "Yes but it was not helpful"),
+                    new Choice("states:service_rating_end1", "No I chose not to subscribe"),
+                    new Choice("states:service_rating_end1", "I didn't know about it")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_end1', function(name){
+            return new ChoiceState(name, {
+                question: "Thanks for rating your circumcision experience. We appreciate your feedback, it will " +
+                "help us improve our MMC service.",
+                choices: [
+                    new Choice("states:main_menu", "Main Menu"),
+                    new Choice("states:end", "Exit")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:service_rating_end2', function(name){
+            return new ChoiceState(name, {
+                question: "Thank you for your interest. We are only looking for ratings from men who have " +
+                "had their circumcision at a clinic recently.",
+                choices: [
+                    new Choice("states:main_menu", "Main Menu"),
+                    new Choice("states:end", "Exit")
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
             });
         });
     });
